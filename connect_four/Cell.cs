b81d7f9d70a70f0eea0364c617644f095d4a66ac
JohnSymeon <125981941+JohnSymeon@users.noreply.github.com>
2023-07-22 19:38:13 +0300

    Update Cell.cs

--- connect_four/Cell.cs
+++ connect_four/Cell.cs
@@ -3,25 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Cell_status{
+    Neutral,
+    Player,
+    Computer
+};
+
+
+
+
+
 [Serializable]
 public class Cell
 {
     
     [SerializeField] public GameObject cell_obj;
-    public int cell_status;
+
+    public Cell_status status;
     public int x_pos;
     public int y_pos;
 
     public Cell()
     {
-        cell_status = 0;
+        status = Cell_status.Neutral;
     }
 
     public void SetCoordinate(int x_pos, int y_pos)
     {
         this.x_pos = x_pos;
         this.y_pos = y_pos;
-       // cell_obj.transform.position = new Vector3(x_pos, y_pos,0);
     }
 
 
