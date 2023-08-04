f4c629126b5631b9a31c9e03e8d0fbb6ce82e16e
JohnSymeon <125981941+JohnSymeon@users.noreply.github.com>
2023-08-04 22:22:51 +0300

    Update MenuScript.cs

--- connect_four/MenuScript.cs
+++ connect_four/MenuScript.cs
@@ -14,9 +14,18 @@ public class MenuScript : MonoBehaviour
     public static int MONTE_NUMBER;
     public static bool PVP_mode;
 
+    public void Start()
+    {
+        FindObjectOfType<AudioManager>().Stop("gameplay_theme");
+        FindObjectOfType<AudioManager>().Play("menu_theme");
+    }
+
     //use to select player vs player game mode
     public void OnPlayPVP()
     {
+        FindObjectOfType<AudioManager>().Play("UI_button");
+        FindObjectOfType<AudioManager>().Stop("menu_theme");
+        FindObjectOfType<AudioManager>().Play("gameplay_theme");
         PVP_mode = true;
         MainMenu.SetActive(false);
         SceneManager.LoadScene(1);
@@ -25,18 +34,23 @@ public class MenuScript : MonoBehaviour
     //use to enter difficulties selection menu
     public void OnPlayCPU()
     {
+        FindObjectOfType<AudioManager>().Play("UI_button");
         MainMenu.SetActive(false);
         Play_CPU.SetActive(true);
     }
     //use to return to the main menu
     public void OnReturn()
     {
+        FindObjectOfType<AudioManager>().Play("UI_button");
         MainMenu.SetActive(true);
         Play_CPU.SetActive(false);
     }
     //use to select a monte carlo number to change the difficulty of the game
     public void OnSelectDifficulty(int number)
     {
+        FindObjectOfType<AudioManager>().Play("UI_button");
+        FindObjectOfType<AudioManager>().Stop("menu_theme");
+        FindObjectOfType<AudioManager>().Play("gameplay_theme");
         MONTE_NUMBER = number;
         SceneManager.LoadScene(1);
     }
