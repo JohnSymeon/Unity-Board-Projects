d4679f242697d4135d5f95bb293b9c4f2d2adefd
JohnSymeon <125981941+JohnSymeon@users.noreply.github.com>
2023-08-06 22:19:10 +0300

    Create asteroid_BG.cs

---
+++ connect_four/asteroid_BG.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteroid_BG : MonoBehaviour
+{
+    private Vector3 pos;
+
+    private float offsety, offsetx;
+
+
+    void Start()
+    {
+        offsety = Random.Range(0f, 10f);
+        offsetx = Random.Range(0f, 10f);
+        pos = transform.position;
+
+    }
+
+
+    void Update()
+    {
+        transform.position = pos + new Vector3(Mathf.Sin(0.1f*Time.time + offsetx), Mathf.Sin(0.1f * Time.time + offsety), 0);
+        //transform.Translate(Vector3.forward * 10 * Time.fixedDeltaTime);
+    }
+
+}
