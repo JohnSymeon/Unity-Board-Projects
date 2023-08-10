271f4377b0a431141fed186078a4144c7a8d9ba0
JohnSymeon <125981941+JohnSymeon@users.noreply.github.com>
2023-08-10 22:12:42 +0300

    Update Mark.cs

--- connect_four/Mark.cs
+++ connect_four/Mark.cs
@@ -24,12 +24,31 @@ public class Mark : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+      void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position,last_played_pos,speed*Time.deltaTime);
         
         Update_GC_On_Status();
+
+        Check_for_Destruction();
     }
+
+    void Check_for_Destruction()
+    {
+        if(GC.game_board.board[(int)transform.position.x, (int)transform.position.y].status==Cell_status.Neutral)
+            Destroy(this);
+
+        if((int)transform.position.x>1 && GC.game_board.board[(int)transform.position.x-1, (int)transform.position.y].status==Cell_status.Neutral)
+        {
+
+            var new_row = GC.game_board.Find_bottom_of_col((int)transform.position.x,(int)transform.position.y);
+
+            last_played_pos = new Vector3((float)new_row, transform.position.y,0f);
+            has_reached_target = false;
+        }
+        
+    }
+
 
     void Update_GC_On_Status()
     {
