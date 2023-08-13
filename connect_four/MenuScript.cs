bf8d068e1f1f323f6d5e47f324caab26e82791d4
JohnSymeon <125981941+JohnSymeon@users.noreply.github.com>
2023-08-13 22:16:38 +0300

    Update MenuScript.cs

--- connect_four/MenuScript.cs
+++ connect_four/MenuScript.cs
@@ -11,58 +11,68 @@ public class MenuScript : MonoBehaviour
 
     public GameObject MainMenu;
     public GameObject Play_CPU;
+    public GameObject MODEs;
     public static int MONTE_NUMBER;
     public static bool PVP_mode;
     public static bool MODE_Tetris;
 
+
     public void OnTetrisToggle()
     {
+        FindObjectOfType<AudioManager>().Play("UI_button");
         if(MODE_Tetris)
             MODE_Tetris=false;
         else
             MODE_Tetris = true;
 
     }
+
     public void Start()
     {
         FindObjectOfType<AudioManager>().Stop("gameplay_theme");
         FindObjectOfType<AudioManager>().Play("menu_theme");
     }
 
-    //use to select player vs player game mode
+    public void OnPickMODES()
+    {
+        FindObjectOfType<AudioManager>().Play("UI_button");
+        MainMenu.SetActive(false);
+        MODEs.SetActive(true);
+    }
+
     public void OnPlayPVP()
     {
+        PVP_mode = true;
+        MainMenu.SetActive(false);
         FindObjectOfType<AudioManager>().Play("UI_button");
         FindObjectOfType<AudioManager>().Stop("menu_theme");
         FindObjectOfType<AudioManager>().Play("gameplay_theme");
-        PVP_mode = true;
-        MainMenu.SetActive(false);
         SceneManager.LoadScene(1);
     }
-    
-    //use to enter difficulties selection menu
+
     public void OnPlayCPU()
     {
         FindObjectOfType<AudioManager>().Play("UI_button");
         MainMenu.SetActive(false);
         Play_CPU.SetActive(true);
     }
-    //use to return to the main menu
+
     public void OnReturn()
     {
         FindObjectOfType<AudioManager>().Play("UI_button");
         MainMenu.SetActive(true);
+        MODEs.SetActive(false);
         Play_CPU.SetActive(false);
+
     }
-    //use to select a monte carlo number to change the difficulty of the game
+
     public void OnSelectDifficulty(int number)
     {
+        MONTE_NUMBER = number;
+        SceneManager.LoadScene(1);
         FindObjectOfType<AudioManager>().Play("UI_button");
         FindObjectOfType<AudioManager>().Stop("menu_theme");
         FindObjectOfType<AudioManager>().Play("gameplay_theme");
-        MONTE_NUMBER = number;
-        SceneManager.LoadScene(1);
     }
-
-    
 }
+
