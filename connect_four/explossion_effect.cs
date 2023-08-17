f56e177b4443b03c2f03b6b21ed1680d80b9cb62
JohnSymeon <125981941+JohnSymeon@users.noreply.github.com>
2023-08-17 22:26:08 +0300

    Create explossion_effect.cs

---
+++ connect_four/explossion_effect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explossion_effect : MonoBehaviour
+{
+    public void Self_Destruct()
+    {
+        Destroy(gameObject);
+    }
+}
