5befc45409fe8980f553787e7241f8884cbb020c
JohnSymeon <125981941+JohnSymeon@users.noreply.github.com>
2023-08-26 22:40:12 +0300

    Update GameController.cs

--- connect_four/GameController.cs
+++ connect_four/GameController.cs
@@ -2,6 +2,8 @@
     This script essentially acts as the game handler during the actual connect four gameplay scene, 
     initiallizing the board and controlling which player has an active turn.
 */
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +11,7 @@ using System;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.UI;
+using System.Threading.Tasks;
 
 public class GameController : MonoBehaviour
 {
@@ -253,7 +256,8 @@ public class GameController : MonoBehaviour
             if(whos_turn_txt.text!="")
                 whos_turn_txt.text = "      turn";
             CPU_is_thinking = true;
-            StartCoroutine(Computer_Played());
+            //StartCoroutine(Computer_Played());
+            Computer_Played_async();
         }
         else
         {
@@ -399,13 +403,35 @@ public class GameController : MonoBehaviour
         //game_board.Roids(UnityEngine.Random.Range(0,game_board.height-1),UnityEngine.Random.Range(0,game_board.width-1));
         yield return null;
     }
+
+    async void Computer_Played_async()
+    {
+        //var rand = new System.Random();
+        //Debug.Log((float)rand.NextDouble());
+        is_dropping = true;
+        await Task.Run(()=>{
+            //await Task.Yield();
+            game_board.AI_Plays(Cell_status.Computer, MenuScript.MONTE_NUMBER);
+            });
+            //Debug.Log("comp plays");
+            Instantiate(computer_mark, new Vector3(game_board.last_played_position.x,8f,0f ) ,transform.rotation);
+            FindObjectOfType<AudioManager>().Play("cpu_plays"); 
+            who_plays = Cell_status.Player;
+            CPU_is_thinking = false;
+            allow_play_coroutine = true;
+            //await Task.Delay(5000);
+            
+        
+    }
+
+
     //use for player's turn
     public void Player_Played(int col)
     {
         if(who_plays==Cell_status.Player)
         {
             buttons[col].reachedMax = game_board.Who_Plays_And_Return_If_Full(Cell_status.Player, col);
-            Instantiate(player_mark, new Vector3(game_board.last_played_position.x,7f,0f ) ,transform.rotation);
+            Instantiate(player_mark, new Vector3(game_board.last_played_position.x,8f,0f ) ,transform.rotation);
             Debug.Log("Player 1 played");
             for(int i=0;i<buttons.Length;i++)
             {
@@ -417,7 +443,7 @@ public class GameController : MonoBehaviour
         else
         {
             buttons[col].reachedMax = game_board.Who_Plays_And_Return_If_Full(Cell_status.Computer, col);
-            Instantiate(computer_mark, new Vector3(game_board.last_played_position.x,7f,0f ) ,transform.rotation);
+            Instantiate(computer_mark, new Vector3(game_board.last_played_position.x,8f,0f ) ,transform.rotation);
             Debug.Log("Player 2 played");
             for(int i=0;i<buttons.Length;i++)
             {
